315049b02d82c13e078b1a77b5d4440367173f8f
Artemonaaa <artemidaabc@gmail.com>
2025-01-05 08:26:27 +0200

    Add typization for webpack.config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,15 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
 
-module.exports = (env) => {
-  return {
+type Mode = 'production' | 'development';
+
+interface EnvVariable {
+  mode: Mode;
+}
+
+export default (env: EnvVariable) => {
+  const config: webpack.Configuration = {
     mode: env.mode ?? 'development',
     entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
@@ -27,5 +33,7 @@ module.exports = (env) => {
     resolve: {
       extensions: ['.tsx', '.ts', '.js'],
     },
-  }
+  };
+
+  return config;
 }
