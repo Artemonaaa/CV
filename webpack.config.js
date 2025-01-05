9fff2c8eb5b37c2e77b407fd062a488cbe46ddbe
Artemonaaa <artemidaabc@gmail.com>
2025-01-05 08:05:26 +0200

    Add simple TypeScript support to Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack');
 module.exports = (env) => {
   return {
     mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
     output: {
       path: path.resolve(__dirname, 'build'),
       filename: '[name].[contenthash].js',
@@ -13,7 +13,19 @@ module.exports = (env) => {
     },
     plugins: [
       new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-      new webpack.ProgressPlugin()
-    ]
+      new webpack.ProgressPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
   }
 }
