98cf4e596ed1da18eb128a1d3269ef975be65706
Artemonaaa <artemidaabc@gmail.com>
2025-01-08 21:06:42 +0200

    Decomposition config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,63 +1,25 @@
-import webpack from 'webpack';
-import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import webpack from "webpack";
+import path from "path";
 
-import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
-
-type Mode = 'production' | 'development';
+import { buildWebpack } from "./config/build/buildWebpack";
+import { BuildMode, BuildPath } from "./config/build/types/types";
 
 interface EnvVariable {
-  mode: Mode;
+  mode: BuildMode;
   port: number;
 }
 
 export default (env: EnvVariable) => {
-  const isDev = env.mode === "development";
-  const isProd = env.mode === "production"
-
-  const config: webpack.Configuration = {
-    mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: '[name].[contenthash].js',
-      clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({template: path.resolve(__dirname, 'public', 'index.html')}),
-      isDev && new webpack.ProgressPlugin(),
-      isProd && new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
-      }), 
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 
-            'css-loader', 
-            'sass-loader'
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    devtool: isDev && 'inline-source-map',
-    devServer: isDev && {
-      port: env.port ?? 3000,
-      open: true  
-    },
+  const paths: BuildPath = {
+    output: path.resolve(__dirname, "build"),
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    html: path.resolve(__dirname, "public", "index.html"),
   };
+  const config: webpack.Configuration = buildWebpack({
+    port: env.port || 3000,
+    mode: env.mode ?? "development",
+    paths,
+  });
 
   return config;
-}
+};
