620467ecd3e09f34436af8a9a053176e1e8589bd
Artemonaaa <artemidaabc@gmail.com>
2025-01-14 21:03:30 +0200

    Add favicon and copyplugin support to webpack confing

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,17 +1,19 @@
 import { Configuration, DefinePlugin, ProgressPlugin } from "webpack";
+import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import CopyPlugin from "copy-webpack-plugin";
 
 import { BuildOptions } from "./types/types";
 
 export function buildPlugins({
-    mode,
-    paths,
+  mode,
+  paths,
   analyzer,
-  platform
+  platform,
 }: BuildOptions): Configuration["plugins"] {
   const isDev = mode === "development";
   const isProd = mode === "production";
@@ -19,6 +21,7 @@ export function buildPlugins({
   const plugins: Configuration["plugins"] = [
     new HtmlWebpackPlugin({
       template: paths.html,
+      favicon: path.resolve(paths.public, "favicon.ico"),
     }),
     new DefinePlugin({
       __PLATFORM__: JSON.stringify(platform),
@@ -28,7 +31,7 @@ export function buildPlugins({
   if (isDev) {
     plugins.push(new ProgressPlugin());
     plugins.push(new ForkTsCheckerWebpackPlugin()),
-    plugins.push(new ReactRefreshWebpackPlugin())
+    plugins.push(new ReactRefreshWebpackPlugin());
   }
 
   if (isProd) {
@@ -36,6 +39,13 @@ export function buildPlugins({
       new MiniCssExtractPlugin({
         filename: "css/[name].[contenthash:8].css",
         chunkFilename: "css/[name].[contenthash:8].css",
+      })
+    );
+    plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+        ],
       })
     );
   }
